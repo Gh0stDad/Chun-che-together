02be627ac110aec7e1aa4443ab3b46d4eaa2960a
Gh0stDad <55903254+Gh0stDad@users.noreply.github.com>
2026-05-01 18:19:01 -0400

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'us-tracker-v3';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install and Cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Serve from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
